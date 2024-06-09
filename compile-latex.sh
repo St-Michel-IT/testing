7cbd575b526e7e3c0b42131a97e27d09df2dfa0e
chrichri <christophe.brun@papit.fr>
2024-06-09 12:35:05 +0200

    Print info in compile-latex.sh.

--- compile-latex.sh
+++ compile-latex.sh
@@ -6,6 +6,10 @@ if ! [ -x "$(command -v lualatex)" ]; then
 fi
 # cd in doc or handle error
 cd doc || exit 1
+# Print the path
+echo "Current Path: $(pwd)"
+# Print files in the current directory
+echo "Files in the current directory: $(ls)"
 # Compile LaTeX with lualatex, compile twice to get the table of contents
 lualatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=build/pdf Tests-Logiciels-Master-MS2D.tex
 lualatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=build/pdf Tests-Logiciels-Master-MS2D.tex
